83e74b8d1a0e073822b41050a6e651c4927558e9
kiriland <kandrieiev@yahoo.com>
2023-04-20 22:40:11 -0400

    MAKE SURE TO MODIFY THE VARIABLES IF YOU HAVE DIFFERENT LOGIN TO MYSQL

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,4 +1,5 @@
 <?php # Establish the database connection via importing this file.
+#Make sure to modify the variables if you have different login, password, or host.
 DEFINE ('DB_USER', 'root');
 DEFINE('DB_PASSWORD','');
 DEFINE('DB_HOST','127.0.0.1');
