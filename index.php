6cc7e41947d55a140c025d643b1dacc791d87843
kiriland <kandrieiev@yahoo.com>
2023-05-02 13:48:14 -0400

    minor fixes

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION['user_id'])){
 				<ul class="navbar-nav ms-md-auto">
 				<button type="button" class="btn btn-outline-primary">Cart <span><?php echo printCart()?></span></button>
 					<li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?php echo $_SESSION['first_name']?>
+                            <?php echo $_SESSION['first_name'] ." ". $_SESSION['last_name']?>
                         </a>
 						<ul class="dropdown-menu">
 							<li><a class="dropdown-item" href="#">Orders</a></li>
