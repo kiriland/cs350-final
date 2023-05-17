19a8e29a5b401c21c628939e6c44585f9282dc57
kiriland <kandrieiev@yahoo.com>
2023-05-17 14:02:04 -0400

    added bg image

--- welcome.php
+++ welcome.php
@@ -7,7 +7,20 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <!-- Custom CSS -->
   <style>
-    /* Add styles for the jumbotron background image */
+    /* Add background image to the website*/
+    html,
+    body {
+        height: 100%;
+    }
+    body {
+        background-image: url('https://wallpapercave.com/wp/wp3152292.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    main {
+        margin-top: 1%;
+    }
+    /* Add styles for the jumbotron background */
     .jumbotron {
       background-color: DarkSlateGrey;
       background-size: cover;
@@ -20,6 +33,18 @@
         width: auto;
         height: 150px;
     }
+
+    /*Footer*/
+    footer {
+        padding: 1%;
+        color: #727272;
+        text-align: center;
+    }
+    footer p:last-child {
+        margin-bottom: 0;
+    }
+
+
   </style>
 </head>
 <body>
@@ -40,15 +65,15 @@
           <ul class="navbar-nav ms-md-auto">
             <button type="button" class="btn btn-outline-primary me-2" onclick='location.href="/login.php"'>Login</button>
             <button type="button" class="btn btn-primary" onclick='location.href="/register.php"'>Sign-up</button>
-            </ul>
-            </li>
+            
           </ul>
         </div>
       </div>
     </nav>
 
+    <main class="container bg-light-subtle mx-auto p-2 rounded">
   <!-- Jumbotron -->
-  <div class="container">
+  <div class="container" style="padding-top: 12px;">
         <div class="p-5 mb-4 rounded-3 jumbotron">
             <div class="container-fluid py-5">
             <h1 class="display-3 fw-bold">Fresh and Affordable Groceries</h1>
@@ -99,6 +124,18 @@
   </div>
   <hr class="featurette-divider">
 </div>
+
+<!-- Footer -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <p>&copy; GreenBean Grocery 2023. All rights reserved.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+</main>
 
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
